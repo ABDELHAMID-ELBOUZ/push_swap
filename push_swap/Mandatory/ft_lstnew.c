7cd3eaafa614db42fb9051c892510c064985f1a3
abdelhamid elbouz <abdelhamidelbouz@gmail.com>
2025-03-10 20:36:30 +0000

    Add files via upload

---
+++ push_swap/Mandatory/ft_lstnew.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aelbouz <aelbouz@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/06 08:46:35 by aelbouz           #+#    #+#             */
+/*   Updated: 2025/02/25 15:45:15 by aelbouz          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../push_swap.h"
+
+t_list	*ft_lstnew(int content)
+{
+	t_list	*node;
+
+	node = (t_list *)malloc(sizeof(t_list));
+	if (node == NULL)
+	{
+		return (NULL);
+	}
+	node->content = content;
+	node->next = NULL;
+	return (node);
+}
